9ecc3236adde8410189cdb53dbf731a5e3404ee2
itsahadul99 <itsahadul99@gmail.com>
2025-07-11 10:54:45 +0600

    project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='font-black text-5xl'>Testing the project with tailwind css</h1>    </>
+  )
+}
+
+export default App
